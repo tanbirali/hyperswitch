79c6e80ed917194b318daac5634ccb870c887ba4
tanbirali <tanbirali2004@gmail.com>
2025-10-24 20:33:52 +0530

    refactorremoved sofort support for shift4

--- crates/hyperswitch_connectors/src/connectors/shift4.rs
+++ crates/hyperswitch_connectors/src/connectors/shift4.rs
@@ -910,16 +910,6 @@ lazy_static! {
             },
         );
 
-        shift4_supported_payment_methods.add(
-            enums::PaymentMethod::BankRedirect,
-            enums::PaymentMethodType::Sofort,
-            PaymentMethodDetails{
-                mandates: enums::FeatureStatus::NotSupported,
-                refunds: enums::FeatureStatus::Supported,
-                supported_capture_methods: supported_capture_methods.clone(),
-                specific_features: None
-            }
-        );
 
         shift4_supported_payment_methods.add(
             enums::PaymentMethod::BankRedirect,
